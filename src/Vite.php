eb0da5180d40a84c2eddebeb6de26b211c32116b
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-01-03 09:37:15 +0530

    build(config): added all devtools configurations

---
+++ src/Vite.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of Codeigniter 4 with Vite.js.
+ *
+ * (c) 2023 Fab IT Hub <hello@fabithub.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Fabithub\ViteJs;
+
+class Vite
+{
+}
