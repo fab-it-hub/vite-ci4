c7ce61c87ff9b511850e982ea1004acf8991854e
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-03-07 21:23:03 +0530

    refactor: :goal_net: Exceptions added

---
+++ src/Exceptions/FileNotFound.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of Codeigniter 4 with Vite.js.
+ *
+ * (c) 2024 Fab IT Hub <hello@fabithub.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Fabithub\ViteJs\Exceptions;
+
+use Exception;
+
+class FileNotFound extends Exception
+{
+}
